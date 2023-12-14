0025cdf90246194660102df72ad09c9cb0a642f2
waris203 <96834098+waris203@users.noreply.github.com>
2023-12-14 20:24:57 +0500

    Add files via upload

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGibm-st0GG0Q-ZSDRHcJtVH1-ILkgJgg",
+  authDomain: "haiderbhai-83eba.firebaseapp.com",
+  projectId: "haiderbhai-83eba",
+  storageBucket: "haiderbhai-83eba.appspot.com",
+  messagingSenderId: "316051628977",
+  appId: "1:316051628977:web:3c163c36f6f23b79729390",
+  measurementId: "G-QJBGVHCHLB"
+};
+
+var app = initializeApp(firebaseConfig);
+console.log(app)
